38e59785424d30088c2d1436ca4d8b6bdc4e87bc
Siddhant Bhatia <siddhant8b@gmail.com>
2024-09-19 00:57:18 +0800

    Revert "Merge dev-website repo (#1)" (#2)
    This reverts commit fa8dfc7572b132be3ba8d854fb7d23ba665d6ce0.

--- dev-website-repo-merge/app/contexts/theme-provider/use-theme.ts
+++
@@ -1,8 +0,0 @@
-import { ThemeContext } from "./theme-provider";
-import { useContext } from "react";
-
-export const useTheme = () => {
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  return { theme, setTheme };
-};
